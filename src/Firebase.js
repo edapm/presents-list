05cc5ce1108b7954a7edd4a2d2e82f0334f1594a
Ed Mason <mynameisprivate3@gmail.com>
2020-10-14 20:04:59 +0100

    Changing everything after this (you were warned!)

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+
+const Init = () => {
+firebase.initializeApp({
+  apiKey: "AIzaSyAAnZX-4yQz4gPdYfOOBqGsiejhs76wEyY",
+  authDomain: "react-presents-list.firebaseapp.com",
+  databaseURL: "https://react-presents-list.firebaseio.com",
+  projectId: "react-presents-list",
+  storageBucket: "react-presents-list.appspot.com",
+  messagingSenderId: "1059174120791",
+  appId: "1:1059174120791:web:1fad922d3ad458d9fe5212",
+  measurementId: "G-HG8Z6K1BHL"
+})
+}
+
+export default Init;
--- src/MyList.js
+++ src/MyList.js
@@ -19,26 +19,57 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-const MyList = () => {
+function MyList() {
   const classes = useStyles();
+  const dummy = useRef();
   const presentsRef = firestore.collection('presents');
-  const query = presentsRef.get()
-  const [presents] = useCollectionData(query);
+  const query = presentsRef.orderBy('createdAt').limit(25);
 
-  return (
-    <div>
-      {presents.map((presents) => {
-        return (
-          <h1 key="presents.name">{presents.name}</h1>
-        )
-      })}
-      <form className={classes.root} autoComplete="off">
-        <TextField id="outlined-basic" label="Present" variant="outlined" autoComplete="off" />
+  const [presents] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendData = async (e) => {
+    e.preventDefault();
+
+    const uid = auth.currentUser;
+
+    await presentsRef.add({
+      text: formValue,
+      uid,
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {presents && presents.map(item => <Item key={item.id} present={item.text} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+      <form className={classes.root} autoComplete="off" onSubmit={sendData} >
+        <TextField id="outlined-basic" label="Present" variant="outlined" autoComplete="off" value={formValue} onChange={(e) => setFormValue(e.target.value)} />
         <Button variant="contained" color="primary" disableElevation>Add Present</Button>
       </form>
+  </>)
+}
+
+
+function Item(props) {
+  const { text, uid } = props.present;
+
+
+  return (<>
+    <div>
+      <p>{text}</p>
     </div>
-  );
-};
+  </>)
+}
 
 export default MyList;
